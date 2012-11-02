8821d30875d30f80423bcf957c900a4f5d1910f2
Philipp Schreiber <phil.bouscher@gmail.com>
2012-11-02 17:07:45 +0100

    hjd

--- src/maere/pinmarkBundle/Controller/latestController.php
+++ src/maere/pinmarkBundle/Controller/latestController.php
@@ -2,22 +2,26 @@
 namespace maere\pinmarkBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class latestController extends Controller{
     public function indexAction(){
         $data=array(
-            
+            'name'=>'Some weired name'
         );
         
-        $content = $engine->render('AcmeArticleBundle:Article:index.html.twig',$data);
+        $content = $this->render('maerepinmarkBundle:Latest:latest.html.twig',$data);
         
-        $response=new Response($content);       
+        $response=new Response($content,'200');       
+        $response->headers->set('Content-Type', 'text/html');
+        $response->setCharset('utf-8');       
         $response->setPublic();
         $response->setPublic();
         $response->setMaxAge(0);
         $response->headers->addCacheControlDirective('must-revalidate', true);
-        $this->render('maereoubnarkBundle:Latest:latest.html.twig',$data);
-        return $response;
+        $response->prepare();
+        $response->sendHeaders();
+        
+        return $content;
     }
 }
 ?>
